9c314649b12e30b246cd3b6b9dbfeaef14940f95
Eitan Joffe <ejoffe@gmail.com>
2021-06-08 10:15:50 -0700

    Commit hook : fix empty commit message handling
    commit-id:dfea232d

--- hook/commithook.go
+++ hook/commithook.go
@@ -11,24 +11,38 @@ import (
 )
 
 func CommitHook(filename string) {
+	if shouldAppendCommitID(filename) {
+		appendCommitID(filename)
+	}
+}
+
+func shouldAppendCommitID(filename string) bool {
 	readfile, err := os.Open(filename)
 	check(err)
 	defer readfile.Close()
 
-	// scan for commit-id - if found do nothing and exit
+	i := 0
+	nonEmptyCommitMessage := false
 	scanner := bufio.NewScanner(readfile)
 	for scanner.Scan() {
-		if strings.HasPrefix(scanner.Text(), "commit-id:") {
-			return
+		line := scanner.Text()
+		if line != "" && !strings.HasPrefix(line, "#") {
+			nonEmptyCommitMessage = true
 		}
+		if strings.HasPrefix(line, "commit-id:") {
+			return false
+		}
+		i++
 	}
 	check(scanner.Err())
-	readfile.Close()
+	return nonEmptyCommitMessage
+}
 
-	// commit-id not found - append a new commit-id to the end of the file
+func appendCommitID(filename string) {
 	appendfile, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0666)
 	check(err)
 	defer appendfile.Close()
+
 	commitID := uuid.New()
 	appendfile.WriteString("\n")
 	appendfile.WriteString(fmt.Sprintf("commit-id:%s\n", commitID.String()[:8]))
