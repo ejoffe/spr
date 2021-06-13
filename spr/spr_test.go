e354c85f5127fed59fdb1358b98b73b6fa50c651
Eitan Joffe <ejoffe@gmail.com>
2021-06-12 20:44:44 -0700

    Unit test for commit amend
    commit-id:90f7eef4

--- spr/spr_test.go
+++ spr/spr_test.go
@@ -123,6 +123,106 @@ func TestSPRBasicFlowFourCommits(t *testing.T) {
 	output.Reset()
 }
 
+func TestSPRAmendCommit(t *testing.T) {
+	assert := require.New(t)
+	cfg := config.Config{
+		RequireChecks:   true,
+		RequireApproval: true,
+	}
+	gitmock := mockgit.NewMockGit(t)
+	githubmock := mockclient.NewMockClient(t)
+	githubmock.Info = &github.GitHubInfo{
+		UserName:     "TestSPR",
+		RepositoryID: "RepoID",
+		LocalBranch:  "master",
+	}
+	var output bytes.Buffer
+	s := NewStackedPR(&cfg, githubmock, gitmock.Cmd, &output, false)
+
+	ctx := context.Background()
+
+	c1 := git.Commit{
+		CommitID:   "00000001",
+		CommitHash: "c100000000000000000000000000000000000000",
+		Subject:    "test commit 1",
+	}
+	c2 := git.Commit{
+		CommitID:   "00000002",
+		CommitHash: "c200000000000000000000000000000000000000",
+		Subject:    "test commit 2",
+	}
+
+	// 'git spr -s' :: StatusPullRequest
+	githubmock.ExpectGetInfo()
+	s.StatusPullRequests(ctx)
+	assert.Equal("", output.String())
+	output.Reset()
+
+	// 'git spr -u' :: UpdatePullRequest :: commits=[c1]
+	githubmock.ExpectGetInfo()
+	gitmock.ExpectFetch()
+	gitmock.ExpectLogAndRespond([]*git.Commit{&c2, &c1})
+	gitmock.ExpectPushCommits([]*git.Commit{&c1, &c2})
+	githubmock.ExpectCreatePullRequest(c1, nil)
+	githubmock.ExpectCreatePullRequest(c2, &c1)
+	githubmock.ExpectGetInfo()
+	s.UpdatePullRequests(ctx)
+	fmt.Printf("OUT: %s\n", output.String())
+	lines := strings.Split(output.String(), "\n")
+	assert.Equal("[✔✔✔✔]   1 : test commit 2", lines[0])
+	assert.Equal("[✔✔✔✔]   1 : test commit 1", lines[1])
+	output.Reset()
+
+	// amend commit c2
+	c2.CommitHash = "c201000000000000000000000000000000000000"
+	// 'git spr -u' :: UpdatePullRequest :: commits=[c1, c2]
+	githubmock.ExpectGetInfo()
+	gitmock.ExpectFetch()
+	gitmock.ExpectLogAndRespond([]*git.Commit{&c2, &c1})
+	gitmock.ExpectPushCommits([]*git.Commit{&c2})
+	githubmock.ExpectUpdatePullRequest(c2, &c1)
+	githubmock.ExpectGetInfo()
+	s.UpdatePullRequests(ctx)
+	lines = strings.Split(output.String(), "\n")
+	fmt.Printf("OUT: %s\n", output.String())
+	assert.Equal("[✔✔✔✔]   1 : test commit 2", lines[0])
+	assert.Equal("[✔✔✔✔]   1 : test commit 1", lines[1])
+	output.Reset()
+
+	// amend commit c1
+	c1.CommitHash = "c101000000000000000000000000000000000000"
+	c2.CommitHash = "c202000000000000000000000000000000000000"
+	// 'git spr -u' :: UpdatePullRequest :: commits=[c1, c2]
+	githubmock.ExpectGetInfo()
+	gitmock.ExpectFetch()
+	gitmock.ExpectLogAndRespond([]*git.Commit{&c2, &c1})
+	gitmock.ExpectPushCommits([]*git.Commit{&c1, &c2})
+	githubmock.ExpectUpdatePullRequest(c1, nil)
+	githubmock.ExpectUpdatePullRequest(c2, &c1)
+	githubmock.ExpectGetInfo()
+	s.UpdatePullRequests(ctx)
+	lines = strings.Split(output.String(), "\n")
+	fmt.Printf("OUT: %s\n", output.String())
+	assert.Equal("[✔✔✔✔]   1 : test commit 2", lines[0])
+	assert.Equal("[✔✔✔✔]   1 : test commit 1", lines[1])
+	output.Reset()
+
+	// 'git spr -m' :: MergePullRequest :: commits=[a1, a2]
+	githubmock.ExpectGetInfo()
+	githubmock.ExpectUpdatePullRequest(c2, nil)
+	githubmock.ExpectMergePullRequest(c2)
+	githubmock.ExpectCommentPullRequest(c1)
+	githubmock.ExpectClosePullRequest(c1)
+	githubmock.ExpectCommentPullRequest(c2)
+	githubmock.ExpectClosePullRequest(c2)
+	s.MergePullRequests(ctx)
+	lines = strings.Split(output.String(), "\n")
+	assert.Equal("MERGED   1 : test commit 1", lines[0])
+	assert.Equal("MERGED   1 : test commit 2", lines[1])
+	fmt.Printf("OUT: %s\n", output.String())
+	output.Reset()
+}
+
 func TestParseLocalCommitStack(t *testing.T) {
 	var buffer bytes.Buffer
 	sd := NewStackedPR(&config.Config{}, nil, nil, &buffer, false)
