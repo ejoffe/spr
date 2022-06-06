ejoffe/spr

commit a8d0e088eb39bf0954967912836cc3c55b084db1

Fix hook installation when in a git worktree
In a git worktree .git is actually a file that points to the location
of the woktree .git directory.  To get the right location for commit
hook installation the --git-common-dir flag needs to be used instead.

commit-id:e3cb2587

hook/install.go
@@ -11,12 +11,12 @@ import (
 )
 
 const (
-	hookPath = ".git/hooks/commit-msg"
+	hookPath = "hooks/commit-msg"
 )
 
 func InstallCommitHook(cfg *config.Config, gitcmd git.GitInterface) {
 	var rootdir string
-	err := gitcmd.Git("rev-parse --show-toplevel", &rootdir)
+	err := gitcmd.Git("rev-parse --git-common-dir", &rootdir)
 	check(err)
 	rootdir = strings.TrimSpace(rootdir)
 	err = os.Chdir(rootdir)

commit d822d7e506473040d1f828f2cffa51e2526e1ffb

Fix critical bug introduced with #206

hook/install.go
@@ -11,15 +11,22 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-const (
-	hookPath = "hooks/commit-msg"
-)
+var hookPath = "hooks/commit-msg"
 
 func InstallCommitHook(cfg *config.Config, gitcmd git.GitInterface) {
 	var rootdir string
 	err := gitcmd.Git("rev-parse --git-common-dir", &rootdir)
 	check(err)
 	rootdir = strings.TrimSpace(rootdir)
+
+	_, err = os.Stat(rootdir)
+	if os.IsNotExist(err) {
+		err = gitcmd.Git("rev-parse --show-toplevel", &rootdir)
+		check(err)
+		rootdir = strings.TrimSpace(rootdir)
+		hookPath = ".git/" + hookPath
+	}
+
 	err = os.Chdir(rootdir)
 	check(err)
 