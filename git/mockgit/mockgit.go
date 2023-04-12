f76baa86abb4dfa1ad6c69fac13b04d7ccec13d7
Eitan Joffe <ejoffe@gmail.com>
2023-04-12 15:30:52 -0700

    Remove user id from pr branch names
    commit-id:a9523952

--- git/mockgit/mockgit.go
+++ git/mockgit/mockgit.go
@@ -73,7 +73,7 @@ func (m *Mock) ExpectPushCommits(commits []*git.Commit) {
 
 	var refNames []string
 	for _, c := range commits {
-		branchName := "pr/TestSPR/master/" + c.CommitID
+		branchName := "spr/master/" + c.CommitID
 		refNames = append(refNames, c.CommitHash+":refs/heads/"+branchName)
 	}
 	m.expect("git push --force --atomic origin " + strings.Join(refNames, " "))
