1046af0ce7c29036ae88f888504619b64f5a7428
Michael Sims <michael.h.sims@gmail.com>
2023-04-23 23:13:08 -0500

    Refactor UpdatePullRequest to accept PR stack instead of GitHubInfo

--- github/mockclient/mockclient.go
+++ github/mockclient/mockclient.go
@@ -81,8 +81,7 @@ func (c *MockClient) CreatePullRequest(ctx context.Context, gitcmd git.GitInterf
 	}
 }
 
-func (c *MockClient) UpdatePullRequest(ctx context.Context, gitcmd git.GitInterface, info *github.GitHubInfo,
-	pr *github.PullRequest, commit git.Commit, prevCommit *git.Commit) {
+func (c *MockClient) UpdatePullRequest(ctx context.Context, gitcmd git.GitInterface, pullRequests []*github.PullRequest, pr *github.PullRequest, commit git.Commit, prevCommit *git.Commit) {
 	fmt.Printf("HUB: UpdatePullRequest\n")
 	c.verifyExpectation(expectation{
 		op:     updatePullRequestOP,
