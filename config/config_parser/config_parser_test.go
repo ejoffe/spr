3d69add6e2fa97337beff49fea1db09c6a8329ee
Simen Brekken <simen.brekken@visma.com>
2024-03-01 22:02:33 +0100

    Expand getRepoDetailsFromRemote tests

--- config/config_parser/config_parser_test.go
+++ config/config_parser/config_parser_test.go
@@ -20,21 +20,30 @@ func TestGetRepoDetailsFromRemote(t *testing.T) {
 		{"origin  https://github.com/r2/d2.git (push)", "github.com", "r2", "d2", true},
 		{"origin  https://github.com/r2/d2.git (fetch)", "", "", "", false},
 		{"origin  https://github.com/r2/d2 (push)", "github.com", "r2", "d2", true},
+		{"origin  https://github.com/r-2/d-2 (push)", "github.com", "r-2", "d-2", true},
 
 		{"origin  ssh://git@github.com/r2/d2.git (push)", "github.com", "r2", "d2", true},
 		{"origin  ssh://git@github.com/r2/d2.git (fetch)", "", "", "", false},
 		{"origin  ssh://git@github.com/r2/d2 (push)", "github.com", "r2", "d2", true},
+		{"origin  ssh://git@github.com/r-2/d-2 (push)", "github.com", "r-2", "d-2", true},
 
 		{"origin  git@github.com:r2/d2.git (push)", "github.com", "r2", "d2", true},
 		{"origin  git@github.com:r2/d2.git (fetch)", "", "", "", false},
 		{"origin  git@github.com:r2/d2 (push)", "github.com", "r2", "d2", true},
+		{"origin  git@github.com:r-2/d-2 (push)", "github.com", "r-2", "d-2", true},
 
 		{"origin  git@gh.enterprise.com:r2/d2.git (push)", "gh.enterprise.com", "r2", "d2", true},
 		{"origin  git@gh.enterprise.com:r2/d2.git (fetch)", "", "", "", false},
 		{"origin  git@gh.enterprise.com:r2/d2 (push)", "gh.enterprise.com", "r2", "d2", true},
+		{"origin  git@gh.enterprise.com:r-2/d-2 (push)", "gh.enterprise.com", "r-2", "d-2", true},
 
 		{"origin  https://github.com/r2/d2-a.git (push)", "github.com", "r2", "d2-a", true},
+		{"origin  https://github.com/r-2/d2-a.git (push)", "github.com", "r-2", "d2-a", true},
 		{"origin  https://github.com/r2/d2_a.git (push)", "github.com", "r2", "d2_a", true},
+		{"origin  https://github.com/r-2/d2_a.git (push)", "github.com", "r-2", "d2_a", true},
+
+		// GitHub names are case-sensitive
+		{"origin  https://github.com/R2/D2.git (push)", "github.com", "R2", "D2", true},
 	}
 	for i, testCase := range testCases {
 		t.Logf("Testing %v %q", i, testCase.remote)
