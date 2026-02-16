49e828507d33d3dcd548b0ade82aed192494bedb
Harsh Shandilya <hshandilya@cloudflare.com>
2026-02-16 12:40:13 +0530

    feat: add common types for all forges
    commit-id:44f1e518

---
+++ forge/pullrequest_test.go
@@ -0,0 +1,181 @@
+package forge
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ejoffe/spr/config"
+	"github.com/ejoffe/spr/git"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMergable(t *testing.T) {
+	type testcase struct {
+		pr     *PullRequest
+		cfg    *config.Config
+		expect bool
+	}
+
+	cfg := func(requireChecks bool, requireApproval bool) *config.Config {
+		return &config.Config{
+			Repo: &config.RepoConfig{
+				RequireChecks:   requireChecks,
+				RequireApproval: requireApproval,
+			},
+		}
+	}
+
+	pr := func(checks checkStatus, approved bool, noConflics bool, stacked bool) *PullRequest {
+		return &PullRequest{
+			MergeStatus: PullRequestMergeStatus{
+				ChecksPass:     checks,
+				ReviewApproved: approved,
+				NoConflicts:    noConflics,
+				Stacked:        stacked,
+			},
+		}
+	}
+
+	tests := []testcase{
+		{pr(CheckStatusUnknown, false, false, false), cfg(false, false), false},
+		{pr(CheckStatusUnknown, false, true, false), cfg(false, false), false},
+		{pr(CheckStatusUnknown, false, true, false), cfg(false, false), false},
+		{pr(CheckStatusUnknown, false, true, true), cfg(false, false), true},
+		{pr(CheckStatusUnknown, false, true, true), cfg(true, false), false},
+		{pr(CheckStatusPending, false, true, true), cfg(true, false), false},
+		{pr(CheckStatusFail, false, true, true), cfg(true, false), false},
+		{pr(CheckStatusPass, false, true, true), cfg(true, false), true},
+		{pr(CheckStatusPass, false, true, true), cfg(true, true), false},
+		{pr(CheckStatusPass, true, true, true), cfg(true, true), true},
+	}
+	for i, test := range tests {
+		assert.Equal(t, test.expect, test.pr.Mergeable(test.cfg), fmt.Sprintf("case %d failed", i))
+	}
+}
+
+func TestReady(t *testing.T) {
+	type testcase struct {
+		pr     *PullRequest
+		cfg    *config.Config
+		expect bool
+	}
+
+	cfg := func(requireChecks bool, requireApproval bool) *config.Config {
+		return &config.Config{
+			Repo: &config.RepoConfig{
+				RequireChecks:   requireChecks,
+				RequireApproval: requireApproval,
+			},
+		}
+	}
+
+	pr := func(checks checkStatus, wip bool, approved bool, noConflics bool, stacked bool) *PullRequest {
+		return &PullRequest{
+			MergeStatus: PullRequestMergeStatus{
+				ChecksPass:     checks,
+				ReviewApproved: approved,
+				NoConflicts:    noConflics,
+				Stacked:        stacked,
+			},
+			Commit: git.Commit{
+				WIP: wip,
+			},
+		}
+	}
+
+	tests := []testcase{
+		{pr(CheckStatusUnknown, false, false, true, false), cfg(false, false), true},
+		{pr(CheckStatusPass, false, true, true, false), cfg(true, false), true},
+		{pr(CheckStatusPass, false, true, false, false), cfg(true, true), false},
+		{pr(CheckStatusFail, false, false, false, false), cfg(true, true), false},
+		{pr(CheckStatusPass, true, false, false, false), cfg(true, true), false},
+		{pr(CheckStatusPass, false, true, false, false), cfg(true, true), false},
+		{pr(CheckStatusPass, false, false, true, false), cfg(true, true), false},
+		{pr(CheckStatusPass, false, false, false, true), cfg(true, true), false},
+	}
+	for i, test := range tests {
+		assert.Equal(t, test.expect, test.pr.Ready(test.cfg), fmt.Sprintf("case %d failed", i))
+	}
+}
+
+func TestStatusString(t *testing.T) {
+	type testcase struct {
+		pr     *PullRequest
+		cfg    *config.Config
+		expect string
+	}
+
+	cfg := func(requireChecks bool, requireApproval bool) *config.Config {
+		return &config.Config{
+			Repo: &config.RepoConfig{
+				RequireChecks:   requireChecks,
+				RequireApproval: requireApproval,
+			},
+			User: &config.UserConfig{
+				StatusBitsEmojis: false,
+			},
+		}
+	}
+
+	pr := func(checks checkStatus, approved bool, noConflics bool, stacked bool) *PullRequest {
+		return &PullRequest{
+			MergeStatus: PullRequestMergeStatus{
+				ChecksPass:     checks,
+				ReviewApproved: approved,
+				NoConflicts:    noConflics,
+				Stacked:        stacked,
+			},
+		}
+	}
+
+	tests := []testcase{
+		{pr(CheckStatusPass, true, true, true), cfg(true, true), "[vvvv]"},
+		{pr(CheckStatusFail, true, true, true), cfg(true, true), "[xvvv]"},
+		{pr(CheckStatusUnknown, true, true, true), cfg(true, true), "[?vvv]"},
+		{pr(CheckStatusPending, true, true, true), cfg(true, true), "[.vvv]"},
+		{pr(CheckStatusPass, false, true, true), cfg(true, true), "[vxvv]"},
+		{pr(CheckStatusPass, true, false, true), cfg(true, true), "[vvxv]"},
+		{pr(CheckStatusPass, true, true, false), cfg(true, true), "[vvvx]"},
+		{pr(CheckStatusPass, true, true, true), cfg(false, true), "[-vvv]"},
+		{pr(CheckStatusPass, true, true, true), cfg(false, false), "[--vv]"},
+	}
+	for i, test := range tests {
+		assert.Equal(t, test.expect, test.pr.StatusString(test.cfg), fmt.Sprintf("case %d failed", i))
+	}
+}
+
+func TestString(t *testing.T) {
+	type testcase struct {
+		pr     *PullRequest
+		cfg    *config.Config
+		expect string
+	}
+
+	cfg := &config.Config{
+		Repo: &config.RepoConfig{
+			RequireChecks:   true,
+			RequireApproval: true,
+		},
+		User: &config.UserConfig{
+			StatusBitsEmojis: false,
+		},
+	}
+
+	pr := func(inQueue bool, commits int) *PullRequest {
+		return &PullRequest{
+			InQueue:     inQueue,
+			Commits:     make([]git.Commit, commits),
+			Title:       "Title",
+			MergeStatus: PullRequestMergeStatus{},
+		}
+	}
+
+	tests := []testcase{
+		{expect: "[?xxx] .   0 : Title", pr: pr(true, 1), cfg: cfg},
+		{expect: "[?xxx] .   0 : Title", pr: pr(true, 2), cfg: cfg},
+		{expect: "[?xxx] !   0 : Title", pr: pr(false, 2), cfg: cfg},
+	}
+	for i, test := range tests {
+		assert.Equal(t, test.expect, test.pr.String(test.cfg), fmt.Sprintf("case %d failed", i))
+	}
+}
